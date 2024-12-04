8af81fa7382f0de4e246edeb19a657bea5a79b8e
Julian402 <corredor.jul@hotmail.com>
2024-12-03 22:17:49 -0500

    Modals

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ModalTypes } from '../../models/modal.types';
+import { Department } from '../../models/department.model';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
+})
+export class ModalComponent {
+  @Input() mode: ModalTypes.DEPARTMENT | ModalTypes.EMPLOYEE =
+    ModalTypes.DEPARTMENT;
+
+  @Output() close = new EventEmitter<void>();
+
+  department: Department = {
+    codeDepartment: 0,
+    nameDepartment: '',
+  };
+
+  employee = {
+    codeEmployee: 0,
+    name: '',
+    lastName1: '',
+    lastName2: '',
+    codeDepartment: 0,
+  };
+
+  save() {
+    if (this.mode === ModalTypes.DEPARTMENT) {
+      //Simulate call backend
+      console.log('save department');
+    } else if (this.mode === ModalTypes.EMPLOYEE) {
+      console.log('save employee');
+    }
+    this.close.emit();
+  }
+}
--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ModalTypes } from '../../models/modal.types';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent, CommonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  ModalTypes = ModalTypes;
+  modalIsOpen = false;
+  modalMode: ModalTypes.DEPARTMENT | ModalTypes.EMPLOYEE =
+    ModalTypes.DEPARTMENT;
 
+  openModal(mode: ModalTypes.DEPARTMENT | ModalTypes.EMPLOYEE) {
+    this.modalMode = mode;
+    this.modalIsOpen = true;
+  }
+  closeModal() {
+    this.modalIsOpen = false;
+  }
 }
